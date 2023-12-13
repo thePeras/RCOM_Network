9698cb2f5651901bdef917e297eb9938a46ed49b
thePeras <jtopereira@gmail.com>
2023-12-13 17:15:24 +0000

    Update setup files

--- lab-setup/switch.sh
+++ lab-setup/switch.sh
@@ -6,10 +6,10 @@
 /interface bridge add name=bridge30
 /interface bridge add name=bridge31
 
-# ether9 = Tux3 eth0
-# ether13 = Tux4 eth0
-# ether14 = Tux2 eth0
-# ether16 = Tux4 eth1
+# ether9 = GNU3 eth0
+# ether13 = GNU4 eth0
+# ether14 = GNU2 eth0
+# ether16 = GNU4 eth1
 
 # Remove default bridge ports
 /interface bridge port remove [find interface = ether9]
@@ -21,6 +21,6 @@
 /interface bridge port add bridge = bidge30 interface = ether13 
 /interface bridge port add bridge = bidge31 interface = ether14
 
-# Adding tux 4 eth1
+# Adding GNU4 eth1
 /interface bridge port remove [find interface =ether16] 
 /interface bridge port add bridge=bidge31 interface=ether16 
